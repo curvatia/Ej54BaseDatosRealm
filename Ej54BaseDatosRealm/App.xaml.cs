682d427c70eb3c6f24e2ec4d6fc9b098447da16f
curvatia <curvatia2002@yahoo.es>
2022-03-04 20:17:13 +0100

    todo

---
+++ Ej54BaseDatosRealm/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ej54BaseDatosRealm
+{
+    public partial class App : Application
+    {
+        public object Navigation { get; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new Views.Inicio());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
